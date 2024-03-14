970ab68225ab701618af1634dca6811620d33cad
Lukáš Petrovický <lukas@petrovicky.net>
2024-03-14 15:14:26 +0100

    ci: fix JMH and Turtle tests

--- src/test/java/ai/timefold/solver/benchmarks/examples/common/TestSystemProperties.java
+++ src/test/java/ai/timefold/solver/benchmarks/examples/common/TestSystemProperties.java
@@ -7,8 +7,8 @@ public final class TestSystemProperties {
      * Use name of the example (eg. "cloudbalancing") or "all" to run all turtle tests.
      * Not providing this property will skip turtle tests.
      */
-    public static final String TURTLE_TEST_SELECTION = "ai.timefold.solver.examples.turtle";
-    public static final String TURTLE_TEST_RUN_TIME_LIMIT = "ai.timefold.solver.examples.turtle.runTimeLimitMinutes";
+    public static final String TURTLE_TEST_SELECTION = "ai.timefold.solver.benchmarks.examples.turtle";
+    public static final String TURTLE_TEST_RUN_TIME_LIMIT = "ai.timefold.solver.benchmarks.examples.turtle.runTimeLimitMinutes";
     public static final String MOVE_THREAD_COUNT = "moveThreadCount";
 
     private TestSystemProperties() {
