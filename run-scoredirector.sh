7b1801b8ffa768687d935386032001aef6fe9187
Lukáš Petrovický <lukas@petrovicky.net>
2024-11-12 17:17:41 +0100

    Don't use taskset for scoredirector benchmarks

--- run-scoredirector.sh
+++ run-scoredirector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo -i sysctl kernel.perf_event_paranoid=1
 sudo -i sysctl kernel.kptr_restrict=0
-taskset -c 0 java -cp target/benchmarks.jar ai.timefold.solver.benchmarks.micro.scoredirector.Main
+java -cp target/benchmarks.jar ai.timefold.solver.benchmarks.micro.scoredirector.Main
